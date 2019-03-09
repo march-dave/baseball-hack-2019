8b533f95ac3f8eb4e1c03e9194c61864bd03db58
march-dave <ysangy@gmail.com>
2019-03-09 14:03:25 -0500

    add more team

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
-import SqareGrid from './SquareGrid';
+import SqareGrid from "./SquareGrid";
+import SqareGrid2 from "./SquareGrid2";
 
 class App extends Component {
   render() {
     return (
-      <SqareGrid />
+      <div>
+        <SqareGrid />
+        <SqareGrid2 />
+      </div>
     );
   }
 }
--- src/SquareGrid2.js
+++ src/SquareGrid2.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import data from "./data.json";
+import data from "./tigerdata.json";
 class SquareGrid extends Component {
 
   componentDidMount() {
